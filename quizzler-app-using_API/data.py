2396bce279df89a60efbeedfe56b4c84cd304f53
Murad <49407545+Murad14@users.noreply.github.com>
2022-11-26 03:58:32 +0600

    final commit

--- quizzler-app-using_API/data.py
+++ quizzler-app-using_API/data.py
@@ -1,11 +1,7 @@
 import requests
 
-# parameters = {
-#     "amount": 10,
-#     "type": "boolean"
-# }
-
-response = requests.get('https://opentdb.com/api.php?amount=10&type=boolean')
+response = requests.get(url='https://opentdb.com/api.php?amount=10&type=boolean')
 response.raise_for_status()
+
 data = response.json()
 question_data = data["results"]
--- quizzler-app-using_API/ui.py
+++ quizzler-app-using_API/ui.py
@@ -1,2 +1,68 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain ):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.quiz_icon = PhotoImage(file="images/icon.png")
+        self.window.iconphoto(False, self.quiz_icon)
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg= THEME_COLOR)
+        self.score_label.grid(row= 0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg = "white")
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     width=280,
+                                                     text="Some Question Text",
+                                                     fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic")
+                                                     )
+        self.canvas.grid(row=1, column=0,columnspan=2, pady=50)
+
+        t_button = PhotoImage(file="images/true.png")
+        self.t_button = Button(image=t_button, highlightthickness=0, borderwidth=0, command= self.true_pressed)
+        self.t_button.grid(row=2, column=0)
+
+        f_button = PhotoImage(file="images/false.png")
+        self.f_button = Button(image=f_button, highlightthickness=0, borderwidth=0, command= self.false_pressed)
+        self.f_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text= q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text= "You've reached the end of the quiz")
+            self.t_button.config(state="disabled")
+            self.f_button.config(state="disabled")
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
 
