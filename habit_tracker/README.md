44c72ec92de26e19625ca87baac339e3d8cc90e0
Murad <49407545+Murad14@users.noreply.github.com>
2022-12-03 02:13:00 +0600

    final commit

---
+++ habit_tracker/README.md
@@ -0,0 +1,8 @@
+# Tracking Graph for Coding using Pixela API
+
+## API link : https://pixe.la/
+
+<br>
+<br>
+
+![Screenshot 2022-12-03 020944](https://user-images.githubusercontent.com/49407545/205377643-e40de07b-aa7c-4588-8b69-85597d654cbd.png)
---
+++ habit_tracker/main.py
@@ -0,0 +1,63 @@
+import requests
+from datetime import datetime
+
+USERNAME = "murad21"
+TOKEN ="fdsjkqj12nuoh**********"
+GRAPH = "graph1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_param = {
+    "token" : TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_param)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": "graph1",
+    "name": "Tracking Graph for Coding",
+    "unit": "commit",
+    "type": "int",
+    "color": "shibafu",
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# response = requests.post(url=graph_endpoint, json= graph_config, headers= headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH}"
+
+today = datetime.now()
+# print(today.strftime("%d,%B,%Y"))
+
+pixel_data = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": input("Input today's total commit: "),
+}
+
+response = requests.post(url=pixel_creation_endpoint, json= pixel_data, headers= headers)
+print(response.text)
+
+update_end_point = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "15"
+}
+
+# response = requests.put(url=update_end_point, json= new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_end_point = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH}/{today.strftime('%Y%m%d')}"
+
+# response = requests.delete(url=delete_end_point, headers=headers)
+# print(response.text)
